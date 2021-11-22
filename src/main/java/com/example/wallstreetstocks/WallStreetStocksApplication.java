3a5983057b7d5474be3c6f6a97692b69e1c3e9f5
Eli <84743591+Eliguajardo@users.noreply.github.com>
2021-11-22 00:03:01 -0600

    Initial commit

---
+++ src/main/java/com/example/wallstreetstocks/WallStreetStocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallstreetstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallStreetStocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallStreetStocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wallstreetstocks/WallStreetStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallstreetstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallStreetStocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
